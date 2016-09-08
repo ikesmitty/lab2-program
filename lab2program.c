03afbb2edbbabd972e2f684332f59ba323a4281a
ikesmitty <smithi@mail.gvsu.edu>
2016-09-08 13:15:11 -0400

    changes made

--- lab2program.c
+++ lab2program.c
@@ -1,10 +1,16 @@
-#include <stdio.h> 
-#include <sys/types.h> 
-#include <unistd.h> 
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char* argv[]) 
-{ 
-    //test
-    return 0; 
+#define SIZE 16
+
+int main()
+{
+    char data1[16];
+
+    printf ("Please input username: ");
+    fgets(data1, SIZE, stdin);
+    printf ("you entered: %s\n", data1);
+    return 0;
 }
+
